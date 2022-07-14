961868bfb3a888bdc7414119fc7ed39de2848b2f
Adzmi Kalnain <adzkalnain@gmail.com>
2022-07-14 09:37:31 +0800

    added: capstone 1 (revised)

---
+++ index.js
@@ -0,0 +1,27 @@
+$(window).on('load', function () {
+    $(document).ready(function() {
+
+        var progress = $('.progressbar .progress')
+
+        function counterInit( fValue, lValue ) {
+
+            var counter_value = parseInt( $('.counter').text() );
+            counter_value++;
+
+            if( counter_value >= fValue && counter_value <= lValue ) {
+                $('.counter').text( counter_value + '%' );
+                progress.css({ 'width': counter_value + '%' });
+
+                setTimeout( function() {
+                    counterInit( fValue, lValue );
+                }, 40 );
+            } else {
+                $(".pre-loader").delay("slow").fadeOut(function() {
+                    $('.pre-loader').hide();
+                });
+            }
+
+        }
+        counterInit( 0, 100 );
+    })
+});
